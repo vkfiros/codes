5ad307d2ba695e86f8420373c2c61caf0072abd4
firosvk <86086392+vkfiros@users.noreply.github.com>
2022-08-22 16:51:27 +0530

    Create create-custom-taxonomy.php

---
+++ wordpress/create-custom-taxonomy.php
@@ -0,0 +1,37 @@
+//hook into the init action and call create_book_taxonomies when it fires
+ 
+add_action( 'init', 'create_subjects_hierarchical_taxonomy', 0 );
+ 
+//create a custom taxonomy name it subjects for your posts
+ 
+function create_subjects_hierarchical_taxonomy() {
+ 
+// Add new taxonomy, make it hierarchical like categories
+//first do the translations part for GUI
+ 
+  $labels = array(
+    'name' => _x( 'Category', 'taxonomy general name' ),
+    'singular_name' => _x( 'Category', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Category' ),
+    'all_items' => __( 'All Category' ),
+    'parent_item' => __( 'Parent Category' ),
+    'parent_item_colon' => __( 'Parent Category' ),
+    'edit_item' => __( 'Edit Category' ), 
+    'update_item' => __( 'Update Category' ),
+    'add_new_item' => __( 'Add New Category' ),
+    'new_item_name' => __( 'New Category Name' ),
+    'menu_name' => __( 'Category' ),
+  );    
+ 
+// Now register the taxonomy
+  register_taxonomy('product_cat',array('products'), array(
+    'hierarchical' => true,
+    'labels' => $labels,
+    'show_ui' => true,
+    'show_in_rest' => true,
+    'show_admin_column' => true,
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'product_cat' ),
+  ));
+ 
+}
